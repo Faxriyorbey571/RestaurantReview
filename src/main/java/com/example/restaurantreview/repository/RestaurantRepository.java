930dc7265d8d0685d18be98960064c88a32ec39e
Faxriyorbey571 <fullstack0705@gmail.com>
2025-08-18 09:42:17 +0500

    added new changes again

--- src/main/java/com/example/restaurantreview/repository/RestaurantRepository.java
+++ src/main/java/com/example/restaurantreview/repository/RestaurantRepository.java
@@ -1,7 +1,12 @@
 package com.example.restaurantreview.repository;
 
+import com.example.restaurantreview.domain.CuisineType;
 import com.example.restaurantreview.domain.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findRestaurants(String searchKeyword, CuisineType cuisineType, Integer minRating, Integer page, Integer size);
+
 }
